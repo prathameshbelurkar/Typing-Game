23f832c577c766301ac4cc8f37fe212c43bba939
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-04-08 11:34:59 +0530

    update README

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { lorem, random } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+
+  enteredText = '';
+
+  onInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.enteredText = target.value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
